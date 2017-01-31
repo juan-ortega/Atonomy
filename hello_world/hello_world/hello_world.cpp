dd55ca2b7308397f066e4885d209e03d13463687
Juan Ortega <juanortega@nevada.unr.edu>
2017-01-30 21:25:14 -0800

    Add files via upload

---
+++ hello_world/hello_world/hello_world.cpp
@@ -0,0 +1,23 @@
+// hello_world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+/*#include <assert.h>
+#include<string.h>
+#include <random>
+#include <time>*/
+
+// #define LYRAND (double)rand()/RAND_MAX
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	int a;
+	a = 0;
+	cin >> a;
+	return 0;
+	
+};
