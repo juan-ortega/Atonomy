347981a953e80ee938fcfd149a55fc8cf44651b5
Juan Ortega <juanortega@nevada.unr.edu>
2017-03-05 23:13:45 -0800

    Add files via upload

---
+++ ProjectBeta.cpp
@@ -0,0 +1,177 @@
+// ProjectBeta.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <assert.h>
+#include <random>
+#include <time.h>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int x = 14;
+int y = 10;
+#define jrand (rand()%x);
+#define orand (rand()%y);
+#define arand double(rand()%1000)/100000;
+
+class agent {
+public:
+	int xpos;
+	int ypos;
+	int position;
+	
+	double nval;
+	double sval;
+	double eval;
+	double wval;
+};
+
+class values {
+public:
+	int xpos;
+	int ypos;
+	double value;
+};
+
+void setup(vector<agent>* pgrid, vector<values>* pprizes, int i, int j, int p, int v) {
+	agent gridspace;
+	values prizevalues;
+
+	gridspace.position = p;
+	gridspace.xpos = i;
+	gridspace.ypos = j;
+	pgrid->push_back(gridspace);
+	gridspace.nval = arand;
+	gridspace.sval = arand;
+	gridspace.eval = arand;
+	gridspace.wval = arand;
+
+	prizevalues.value = v;
+	prizevalues.xpos = i;
+	prizevalues.ypos = j;
+	pprizes->push_back(prizevalues);
+}
+
+int coord(int xcoord, int ycoord) {
+	int z = 0;
+	if (xcoord < 0) {
+		return -1;
+	}
+	else if (ycoord < 0) {
+		return -1;
+	}
+
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < y; j++) {
+			if ((i == xcoord) && (j == ycoord)) {
+				return z;
+			}
+			else {
+				z = z + 1;
+			}
+		}
+	}
+	return (x*y);
+}
+
+int ipos(int index) {
+	int z = 0;
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < y; j++) {
+			if (z == index) {
+				return i;
+			}
+			else {
+				z = z + 1;
+			}
+		}
+	}
+}
+
+int jpos(int index) {
+	int z = 0;
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < y; j++) {
+			if (z == index) {
+				return j;
+			}
+			else {
+				z = z + 1;
+			}
+		}
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	vector<agent> grid;
+	vector<agent>* pgrid = &grid;
+	vector<values> prizes;
+	vector<values>* pprizes = &prizes;
+
+	int xposition = jrand;
+	int yposition = orand;
+	int xprize = jrand;
+	int yprize = orand;
+	cout << "Prize is at " << xprize << "," << yprize << endl; 
+	cout << "Agent is at " << xposition << "," << yposition << endl;
+
+///////////////////////////////////
+//GRID AND AGENT SET UP
+	int p;
+	int v;
+
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < y; j++) {
+			if ((i == xposition) && (j == yposition)) {
+				p = 1;
+				v = -1;
+				setup(pgrid, pprizes, i, j, p, v);
+				//cout << "AP" << i << "," << j << endl;
+			}
+			else if ((i == xprize) && (j == yprize)) {
+				p = 0;
+				v = 100;
+				setup(pgrid, pprizes, i, j, p, v);
+				//cout << "GP" << i << "," << j << endl;
+			}
+			else {
+				p = 0;
+				v = -1;
+				setup(pgrid, pprizes, i, j, p, v);
+			}
+		}
+	}
+//END OF SET UP
+////////////////////////////////////////////////
+//Q-LEARNING AGENT 
+	int cycles = 1000;
+	int agentcoord = coord(xposition, yposition);
+	for (int m = 0; m < cycles; m++) {
+		
+		//value(
+		//learn(
+	}
+
+//END OF Q-LEARNING AGENT
+////////////////////////////////////////////////
+//
+
+
+
+
+
+	
+	int a = 0;
+	int j = 0;
+	cin >> a;
+	assert(a == j);
+	return 0;
+	return 0;
+}
+
